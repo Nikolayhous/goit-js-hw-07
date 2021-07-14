3c17ffdf166562f4e6752be61465db3f7f2dccc6
Николай Гребенюк <Yanukhous@ukr.net>
2021-07-14 14:45:20 +0300

    Добавив рішення 8 задачі

--- js/task-08.js
+++ js/task-08.js
@@ -1,35 +1,35 @@
-// const createBtn = document.querySelector('[data-action="render"]');
-// // console.log(createBtn);
-// const clearBtn = document.querySelector('[data-action="destroy"]');
-// // console.log(clearBtn);
-// const divBoxes = document.getElementById('boxes');
+const createBtn = document.querySelector('[data-action="render"]');
+// console.log(createBtn);
+const clearBtn = document.querySelector('[data-action="destroy"]');
+// console.log(clearBtn);
+const divBoxes = document.getElementById('boxes');
 
-// function getAmount() {
-//   const amount = +document.querySelector('#controls input').value;
-//   createBoxes(amount);
-// }
+function getAmount() {
+  const amount = +document.querySelector('#controls input').value;
+  createBoxes(amount);
+}
 
-// // console.log(divEl);
-// // console.log(divBoxes);
+// console.log(divEl);
+// console.log(divBoxes);
 
-// function createBoxes(amount) {
-//   let size = 30;
-//   const fragment = document.createDocumentFragment();
-//   for (let i = 0; i < amount; i += 1) {
-//     let newSize = size + i * 10;
-//     const divEl = document.createElement('div');
-//     divEl.style.cssText = `width: ${newSize}px; height: ${newSize}px; background-color: rgba(${random()}, ${random()}, ${random()})`;
-//     fragment.appendChild(divEl);
-//   }
-//   divBoxes.appendChild(fragment);
-// }
+function createBoxes(amount) {
+  let size = 30;
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i += 1) {
+    let newSize = size + i * 10;
+    const divEl = document.createElement('div');
+    divEl.style.cssText = `width: ${newSize}px; height: ${newSize}px; background-color: rgba(${random()}, ${random()}, ${random()})`;
+    fragment.appendChild(divEl);
+  }
+  divBoxes.appendChild(fragment);
+}
 
-// const destroyBoxes = () => {
-//   divBoxes.innerHTML = '';
-// };
+const destroyBoxes = () => {
+  divBoxes.innerHTML = '';
+};
 
-// function random() {
-//   return Math.floor(Math.random() * 256);
-// }
-// createBtn.addEventListener('click', getAmount);
-// clearBtn.addEventListener('click', destroyBoxes);
+function random() {
+  return Math.floor(Math.random() * 256);
+}
+createBtn.addEventListener('click', getAmount);
+clearBtn.addEventListener('click', destroyBoxes);
